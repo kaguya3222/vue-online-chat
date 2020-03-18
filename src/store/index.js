d37e42d43c7887287fe014b3e2f394054c7d273e
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-18 03:27:42 +0200

    Added messages logic to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,43 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    messages: []
+  },
+  getters: {
+    messages(state) {
+      return state.messages;
+    }
+  },
+  mutations: {
+    appendToMessages(state, { message }) {
+      state.messages.push(message);
+    },
+    removeFromMessages(state, { index }) {
+      state.messages.splice(index, 1);
+    },
+    updateMessages(state, { payload }) {
+      Vue.set(state.messages, payload.index, payload.message);
+    }
+  },
+  actions: {
+    appendMessage(store, { message }) {
+      store.commit("appendToMessages", { message });
+    },
+    removeMessage(store, { messageId }) {
+      const index = store.state.messages.findIndex(
+        message => message.id === messageId
+      );
+      store.commit("removeFromMessages", { index });
+    },
+    updateMessage(store, { updatedMessage }) {
+      const index = store.state.messages.findIndex(
+        message => message.id === updatedMessage.id
+      );
+      store.commit("updateMessages", {
+        payload: { message: updatedMessage, index }
+      });
+    }
+  },
   modules: {}
 });
