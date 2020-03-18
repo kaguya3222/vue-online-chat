c9acca1b3e5a5f3e4c6a0be4970fe3b8f2fc37b5
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-18 03:29:01 +0200

    Created ChatMessageLis component

---
+++ src/components/ChatMessageList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <ChatMessageListItem
+      v-for="(message, index) in messages"
+      :key="index"
+      :message="message"
+    />
+  </div>
+</template>
+
+<script>
+import ChatMessageListItem from "./ChatMessageListItem";
+import { mapGetters } from "vuex";
+export default {
+  name: "ChatMessages.vue",
+  computed: {
+    ...mapGetters(["messages"])
+  },
+  components: {
+    ChatMessageListItem
+  }
+};
+</script>
+
+<style scoped></style>
