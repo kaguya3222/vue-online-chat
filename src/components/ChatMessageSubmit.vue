2377535010b0071d017893710f9b0fc095fc07ec
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-18 03:29:23 +0200

    Created ChatMessageSubmit component

---
+++ src/components/ChatMessageSubmit.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="storeMessage">
+    <div class="form-group">
+      <label>Message:</label>
+      <textarea v-model="messageText" class="form-control"></textarea>
+    </div>
+    <div class="form-group">
+      <label>Nickname:</label>
+      <input v-model="nickname" class="form-control" />
+    </div>
+    <button class="btn btn-primary">Send</button>
+  </form>
+</template>
+
+<script>
+import { messagesRef } from "../firebaseConfig";
+export default {
+  name: "ChatMessageSubmit",
+  data() {
+    return {
+      messageText: "",
+      nickname: "hootlex"
+    };
+  },
+  methods: {
+    storeMessage() {
+      messagesRef.push({ text: this.messageText, nickname: this.nickname });
+      this.messageText = "";
+    }
+  }
+};
+</script>
+
+<style scoped></style>
