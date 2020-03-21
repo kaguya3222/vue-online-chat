ab3977920bbac5eaaf35380f7dd65e1e09b38ba1
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-21 03:32:29 +0200

    Created message for non-authorized users in PageChat.vue

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -3,17 +3,22 @@
     <ChatMessageList />
     <hr />
     <ChatMessageSubmit />
+    <h2 class="text-center" v-if="!authUser">
+      In order to start chatting, please, sign in!
+    </h2>
   </div>
 </template>
 
 <script>
 import ChatMessageList from "../components/PageChatMessageList";
 import ChatMessageSubmit from "../components/PageChatMessageSubmitForm";
-import { messagesRef } from "../firebaseConfig";
 import { mapGetters } from "vuex";
 
 export default {
   name: "PageChat.vue",
+  computed: {
+    ...mapGetters(["authUser"])
+  },
   components: {
     ChatMessageSubmit,
     ChatMessageList
