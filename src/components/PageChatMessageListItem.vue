7d5b32c9861256c984fb9e116c72a4deed6f7e8a
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-21 03:35:14 +0200

    Modified template in PageChatMessageListItem.vue

--- src/components/PageChatMessageListItem.vue
+++ src/components/PageChatMessageListItem.vue
@@ -2,38 +2,58 @@
   <div class="card">
     <div class="card-body">
       <h6 class="card-subtitle mb-2 text-muted">{{ message.nickname }}</h6>
-      <p class="card-text">{{ message.text }}</p>
+      <div class="d-flex flex-row">
+        <div class="d-flex flex-column">
+          <span class="card-text">{{ message.text }}</span>
+          <div v-if="isAuthUserMessage">
+            <div class="mt-2" v-if="!areMessagesMatch(message)">
+              <a
+                @click.prevent="deleteMessage(message)"
+                href="#"
+                class="card-link"
+                >Delete</a
+              >
+              <a
+                @click.prevent="beginEditingMessage(message)"
+                href="#"
+                class="card-link"
+                >Edit</a
+              >
+            </div>
+            <template v-else>
+              <a
+                @click.prevent="updateMessage(message)"
+                href="#"
+                class="card-link"
+                >Update</a
+              >
+              <a @click.prevent="cancelEditing" href="#" class="card-link"
+                >Cancel</a
+              >
+            </template>
+          </div>
+        </div>
+        <div class="ml-auto text-secondary mt-auto">
+          <AppDate :unix-date="message.publishedAt" />
+        </div>
+      </div>
       <textarea
         v-model="editingMessage.text"
         class="form-control"
         v-if="areMessagesMatch(message)"
       ></textarea>
-      <template v-if="!areMessagesMatch(message)">
-        <a @click.prevent="deleteMessage(message)" href="#" class="card-link"
-          >Delete</a
-        >
-        <a
-          @click.prevent="beginEditingMessage(message)"
-          href="#"
-          class="card-link"
-          >Edit</a
-        >
-      </template>
-      <template v-else>
-        <a @click.prevent="updateMessage(message)" href="#" class="card-link"
-          >Update</a
-        >
-        <a @click.prevent="cancelEditing" href="#" class="card-link">Cancel</a>
-      </template>
     </div>
   </div>
 </template>
 
 <script>
 import { messagesRef } from "../firebaseConfig";
+import { mapGetters } from "vuex";
+import AppDate from "./AppDate";
 
 export default {
   name: "PageChatMessageListItem",
+  components: { AppDate },
   props: {
     message: {
       required: true,
@@ -44,6 +64,12 @@ export default {
     return {
       editingMessage: null
     };
+  },
+  computed: {
+    ...mapGetters(["authUser"]),
+    isAuthUserMessage() {
+      return this.authUser ? this.message.uid === this.authUser.uid : null;
+    }
   },
   methods: {
     areMessagesMatch(message) {
