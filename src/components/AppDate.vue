04fec64ad219bca0acee8cdf7504c9c41a26de22
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-21 03:33:46 +0200

    Created AppDate.vue

---
+++ src/components/AppDate.vue
@@ -0,0 +1,26 @@
+<template>
+  <span :title="unixDate | humanFriendlyDate">
+    {{ unixDate | relativeDate }}
+  </span>
+</template>
+
+<script>
+import moment from "moment";
+export default {
+  name: "AppDate.vue",
+  props: {
+    unixDate: {
+      required: true,
+      type: Number
+    }
+  },
+  filters: {
+    humanFriendlyDate(date) {
+      return moment.unix(date).format("MMMM Do YYYY, h:mm:ss a");
+    },
+    relativeDate(date) {
+      return moment.unix(date).fromNow();
+    }
+  }
+};
+</script>
