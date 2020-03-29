914c23d9f70caf159ae88aaafb493099a5a29318
kaguya3222 <maksimanisimov322@gmail.com>
2020-03-29 18:23:50 +0300

    Added validation for PageAuthorization.vue

--- src/pages/PageAuthorization.vue
+++ src/pages/PageAuthorization.vue
@@ -2,16 +2,14 @@
   <div
     class="d-flex flex-column justify-content-center align-items-center h-100"
   >
-    <b-form
-      @submit.prevent="authorizeAndMoveToHomePage"
-      class="d-flex flex-column"
-    >
+    <b-form @submit.prevent="authorize" class="d-flex flex-column">
       <b-form-group label="Email: " label-for="email">
         <b-form-input
           id="email"
           type="email"
           placeholder="Email"
-          v-model="form.email"
+          :state="isFormValid"
+          v-model="$v.form.email.$model"
         />
       </b-form-group>
       <b-form-group label="Password: " label-for="password">
@@ -19,7 +17,8 @@
           id="password"
           type="password"
           placeholder="Password"
-          v-model="form.password"
+          v-model="$v.form.password.$model"
+          :state="isFormValid"
           autocomplete="cc-password"
         ></b-form-input>
       </b-form-group>
@@ -33,36 +32,63 @@
         <template v-else>Sign In</template>
       </b-button>
     </b-form>
+    <span v-if="isAuthorizingFailed" class="text-danger mt-1"
+      >Invalid email or password</span
+    >
   </div>
 </template>
 
 <script>
 import { firebaseAuthentication } from "../firebaseConfig";
 import page from "../mixins/page";
+import { validationMixin } from "vuelidate";
+import { required } from "vuelidate/lib/validators";
 
 export default {
-  mixins: [page],
+  mixins: [page, validationMixin],
   data() {
     return {
       form: {
         email: "",
         password: ""
       },
-      isAuthorizing: false
+      isAuthorizing: false,
+      isAuthorizingFailed: false
     };
   },
+  validations: {
+    form: {
+      email: { required },
+      password: { required },
+      isValid() {
+        return !this.isAuthorizingFailed;
+      }
+    }
+  },
+  computed: {
+    isFormValid() {
+      return this.$v.form.isValid ? null : false;
+    }
+  },
   methods: {
-    async authorizeAndMoveToHomePage() {
-      await this.authorize();
-      this.moveToHomePage();
-    },
     async authorize() {
       this.isAuthorizing = true;
-      await firebaseAuthentication.signInWithEmailAndPassword(
-        this.form.email,
-        this.form.password
-      );
+      await firebaseAuthentication
+        .signInWithEmailAndPassword(this.form.email, this.form.password)
+        .then(() => {
+          this.authorizingIsPassed();
+        })
+        .catch(() => {
+          this.authorizingIsFailed();
+        });
       this.isAuthorizing = false;
+    },
+    authorizingIsFailed() {
+      this.isAuthorizingFailed = true;
+    },
+    authorizingIsPassed() {
+      this.isAuthorizingFailed = false;
+      if (!this.isAuthorizingFailed) this.moveToHomePage();
     }
   }
 };
