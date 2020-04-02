34588c0bb53442a596526576366c0b120a2ccf31
kaguya3222 <maksimanisimov322@gmail.com>
2020-04-02 16:31:35 +0300

    Added message post notification

--- src/pages/PageChat.vue
+++ src/pages/PageChat.vue
@@ -7,6 +7,7 @@
       class="align-self-center"
     ></b-spinner>
     <div v-else>
+      <h2 v-if="isAtLeastOneMessagePosted">Be the first to post a message!</h2>
       <ChatMessageList />
       <hr />
       <ChatMessageSubmit />
@@ -21,14 +22,73 @@
 import ChatMessageList from "../components/PageChatMessageList";
 import ChatMessageSubmit from "../components/PageChatFormMessageSubmit";
 import { mapGetters } from "vuex";
+import notifications from "../mixins/notifications";
 
 export default {
   components: {
     ChatMessageSubmit,
     ChatMessageList
   },
+  mixins: [notifications],
+  data() {
+    return {
+      messagesLengthBeforePost: null,
+      isLoading: true
+    };
+  },
   computed: {
-    ...mapGetters(["authUser", "areMessagesLoaded"])
+    ...mapGetters([
+      "userByProperty",
+      "messages",
+      "nickname",
+      "areMessagesLoaded",
+      "authUser"
+    ]),
+    shouldPostNotificationBeShown() {
+      return this.isMessageNew && this.isMessagePostedByAnotherUser;
+    },
+    isMessageNew() {
+      return this.messages.length > this.messagesLengthBeforePost;
+    },
+    isMessagePostedByAnotherUser() {
+      return this.userThatPublishedMessage.nickname !== this.nickname;
+    },
+    userThatPublishedMessage() {
+      return this.userByProperty({
+        property: { name: "uid", value: this.lastPublishedMessage.uid }
+      });
+    },
+    lastPublishedMessage() {
+      return this.messages[this.messages.length - 1];
+    },
+    isAtLeastOneMessagePosted() {
+      return this.messages.length === 0;
+    }
+  },
+  watch: {
+    areMessagesLoaded: {
+      handler(value) {
+        if (value) {
+          this.messagesLengthBeforePost = this.messages.length;
+        }
+      }
+    },
+    messages: {
+      handler(value) {
+        this.showMessagePostNotification();
+        this.messagesLengthBeforePost = value.length;
+      }
+    }
+  },
+  methods: {
+    showMessagePostNotification() {
+      if (this.shouldPostNotificationBeShown) {
+        this.showNotification({
+          message: `${this.userThatPublishedMessage.nickname} just posted a message!`,
+          type: "success"
+        });
+      }
+    }
   }
 };
 </script>
