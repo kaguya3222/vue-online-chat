b29aa7e862f72fbca7615f68c765d0173541ef04
kaguya3222 <maksimanisimov322@gmail.com>
2020-04-02 16:54:16 +0300

    Moved firebase config to firebaseTools.js

--- src/firebaseTools.js
+++ src/firebaseTools.js
@@ -1,7 +1,16 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
-import firebaseConfig from "../private/firebaseConfig";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCoVt2-c1pp_GS8RTa2fIxk1fFntZu6-kk",
+  authDomain: "vue-online-chat-98a54.firebaseapp.com",
+  databaseURL: "https://vue-online-chat-98a54.firebaseio.com",
+  projectId: "vue-online-chat-98a54",
+  storageBucket: "vue-online-chat-98a54.appspot.com",
+  messagingSenderId: "602010413819",
+  appId: "1:602010413819:web:ad53a9cbea0c8f5e48de5e"
+};
 
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
